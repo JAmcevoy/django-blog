a9d5c95af2ea90a947b2552d8e4e380efc02d91e
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-04-12 09:54:53 +0000

    Added templates, base, index & static, style.css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,5 +7,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-
-    # template = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
