6574b7d46ef000f31e053c99bfffdf3f037ee6e2
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-04-16 10:20:35 +0000

    disable debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jamcevoy-djangoblog-gh7nzatbhcs.ws-eu110.gitpod.io', '.herokuapp.com']
 
