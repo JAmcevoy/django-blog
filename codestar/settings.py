457eb3aa23da5a1f11fb6fc7ccf4ef5c9c0a6823
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-04-18 09:06:03 +0000

    Save after lock out

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jamcevoy-djangoblog-gh7nzatbhcs.ws-eu110.gitpod.io', '.herokuapp.com']
 
@@ -49,15 +49,19 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
+    'blog',
     'about',
 ]
-
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
